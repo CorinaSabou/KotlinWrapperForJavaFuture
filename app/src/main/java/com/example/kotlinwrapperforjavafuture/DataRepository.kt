23b360dbb3e87a601c637de7e65c21c99ed98f20
Corina Sabou <corina.sabou@storytel.com>
2025-05-06 16:37:53 +0200

    Added timeout to the repo

--- app/src/main/java/com/example/kotlinwrapperforjavafuture/DataRepository.kt
+++ app/src/main/java/com/example/kotlinwrapperforjavafuture/DataRepository.kt
@@ -1,37 +1,41 @@
 package com.example.kotlinwrapperforjavafuture
 
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeout
 
 class DataRepository {
 
-    suspend fun kotlinFunctionWithEmptyContinuation() =
-        suspendCancellableCoroutine { cont ->
-            ContinuationsPlayground.functionWithEmptyContinuation {
-                if (cont.isActive && !cont.isCompleted) {
-                    cont.resumeWith(Result.success(Unit))
+    suspend fun kotlinFunctionWithEmptyContinuation(timeOutMs: Long = 200) =
+        withTimeout(timeOutMs) {
+            suspendCancellableCoroutine { cont ->
+                ContinuationsPlayground.functionWithEmptyContinuation {
+                    if (cont.isActive && !cont.isCompleted) {
+                        cont.resumeWith(Result.success(Unit))
+                    }
                 }
             }
         }
 
-    suspend fun kotlinFunctionWithIntContinuation(): Int {
-        return suspendCancellableCoroutine { cont ->
-            ContinuationsPlayground.functionWithIntContinuation({ result ->
-                if (cont.isActive && !cont.isCompleted) {
-                    cont.resumeWith(Result.success(result))
-                }
-            })
+    suspend fun kotlinFunctionWithIntContinuation(timeOutMs: Long = 200): Int =
+        withTimeout(timeOutMs) {
+            suspendCancellableCoroutine { cont ->
+                ContinuationsPlayground.functionWithIntContinuation({ result ->
+                    if (cont.isActive && !cont.isCompleted) {
+                        cont.resumeWith(Result.success(result))
+                    }
+                })
+            }
         }
-    }
 
-    suspend fun kotlinFunctionWithByteArrayContinuation(byteArray: ByteArray): ByteArray {
-        return suspendCancellableCoroutine { cont ->
-            ContinuationsPlayground.functionWithByteArrayContinuation(byteArray, { result ->
-                if (cont.isActive && !cont.isCompleted) {
-                    cont.resumeWith(Result.success(result))
-                }
-            })
+    suspend fun kotlinFunctionWithByteArrayContinuation(byteArray: ByteArray, timeOutMs: Long = 200): ByteArray =
+        withTimeout(timeOutMs) {
+            suspendCancellableCoroutine { cont ->
+                ContinuationsPlayground.functionWithByteArrayContinuation(byteArray, { result ->
+                    if (cont.isActive && !cont.isCompleted) {
+                        cont.resumeWith(Result.success(result))
+                    }
+                })
+            }
         }
-    }
-
 
 }
